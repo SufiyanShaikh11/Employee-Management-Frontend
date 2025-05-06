8984cd65428b638e2394ce8a0c34094c9a4b73d1
sufiyan <srsufiyan1@gmail.com>
2025-05-06 19:37:59 +0530

    Updated config and integrated backend

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://employee-management-backend-production-2a33.up.railway.app/"
+    ? "https://employee-management-backend-production-2a33.up.railway.app"
     : "http://localhost:8080";
 
 export default BASE_URL;
